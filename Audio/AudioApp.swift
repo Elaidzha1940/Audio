476ba7f5e5412926210572f1570c0dbab14862a4
Elaidzha1940 <eli080293@gmail.com>
2023-08-14 14:16:01 +0300

    Audio

---
+++ Audio/AudioApp.swift
@@ -0,0 +1,21 @@
+//  /*
+//
+//  Project: Audio
+//  File: AudioApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 14.08.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+@main
+struct AudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Audio/ContentView.swift
@@ -0,0 +1,30 @@
+//  /*
+//
+//  Project: Audio
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 14.08.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
